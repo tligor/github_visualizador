6d39c73e0138289c8f290be17588642c892d8b2a
Igor de Almeida Santana <igor-almeida.santana@rfb.gov.br>
2024-10-24 16:16:16 -0300

    adicionada verificação de existencia de usuário no Github

--- src/components/reposlist/index.jsx
+++ src/components/reposlist/index.jsx
@@ -4,16 +4,30 @@ import styles from './reposlist.module.css'
 const ReposList = ({nomeUsr})=>{
     const [repos, setRepos] = useState([]);
     const [estaCarregando, setEstaCarregando] = useState(true);
-
+    const [throwErr, setThrowErr] = useState('');
     useEffect(()=> {
         setEstaCarregando(true);
+        setThrowErr('');
         fetch(`https://api.github.com/users/${nomeUsr}/repos`)
-        .then(res => res.json())
+        .then(res => {
+            if(!res.ok){
+                if (res.status === 404){
+                    throw new Error('Usuário não encontrado');
+                }else {
+                    throw new Error('Erro ao carregar os repositórios');
+                }
+            }
+            return res.json();
+        })
         .then(resJson => {
             setTimeout(()=> {
                 setEstaCarregando(false)
                 setRepos(resJson);
             },1000)
+        }).catch(e => {
+            setEstaCarregando(false)
+            setThrowErr(e.message)
+            console.error(e)
         })
     },[nomeUsr])
 
@@ -21,6 +35,11 @@ const ReposList = ({nomeUsr})=>{
         <div className="container">
             {estaCarregando ? (
                 <h1>Carregando...</h1>
+            ) : throwErr ? (
+                <div className={styles.throwError}>
+                    <h1 className={styles.throwErrorTitle}>Um erro foi encontrado</h1>
+                    <p className={styles.throwErrorMessage}>{throwErr}, verifique e tente novamente.</p>
+                </div>
             ) : (
                 <ul className={styles.list}>
                 {repos.map(({id, name, language, html_url}) => (
